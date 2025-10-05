08a12c50e37dd0c11a5f0a4f83c363fc6b70e6d6
weekend-game <weekend_game@mail.ru>
2025-10-05 21:27:09 +0300

    Empty Maven project

---
+++ src/main/java/game/weekend/SeedKeeperEN/SystemInfo.java
@@ -0,0 +1,13 @@
+package game.weekend.SeedKeeperEN;
+
+public class SystemInfo {
+
+    public static String javaVersion() {
+        return System.getProperty("java.version");
+    }
+
+    public static String javafxVersion() {
+        return System.getProperty("javafx.version");
+    }
+
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,4 @@
+module game.weekend.SeedKeeperEN {
+    requires javafx.controls;
+    exports game.weekend.SeedKeeperEN;
+}
